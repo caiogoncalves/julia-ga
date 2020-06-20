2ae31369da802d009f460a6d3515597f7b3baa79
Caio Goncalves <caioagoncalves@gmail.com>
2020-06-20 17:22:21 +0200

    Criada a funçao ga_extract

--- ga_teste.jl
+++ ga_teste.jl
@@ -4,29 +4,51 @@ using Tables
 using JSON3
 using DataFrames
 using JSONTables
+using Dates
 
-creds = JSONCredentials("credentials.json")
-session = GoogleSession(creds, ["analytics.readonly"])
-auth = authorize(session)
+id = "148386669"
+Start_date = "2020-06-10"
+End_date = "2020-06-19"
+metrics = ["sessions","users"]
+Dimensions = ["date", "source", "medium"]
+
+function ga_extract(id,Start_date,End_date,metrics,Dimensions)
+    creds = JSONCredentials("credentials.json")
+    session = GoogleSession(creds, ["analytics.readonly"])
+    auth = authorize(session)
     headers = Dict{String, String}(
         "Authorization" => "$(auth[:token_type]) $(auth[:access_token])"
     )
 
-url = "https://www.googleapis.com/analytics/v3/management/accounts/"
-url_get_data = "https://www.googleapis.com/analytics/v3/data/ga?ids=ga:148386669&start-date=2020-06-10&end-date=2020-06-13&metrics=ga:sessions&dimensions=ga:date"
+    url = "https://www.googleapis.com/analytics/v3/management/accounts/"
 
-requisicao = HTTP.request("GET", url, headers)
-requisicao_report = HTTP.request("GET", url_get_data, headers)
-#println(String(requisicao_report.body))
-tabela = String(requisicao_report.body)
-tabela = JSON3.read(tabela)
-tabela = tabela[:rows]
-dia = zeros()
-sessoes = zeros()
-for i in tabela
-    dia = push!(dia, i[1])
-    sessoes = append!(sessoes ,i[2])
+
+    query_metrics = join(metrics,",ga:")
+    query_dimensions = join(Dimensions,",ga:")
+    url_get_data = "https://www.googleapis.com/analytics/v3/data/ga?ids=ga:"*
+        id*"&start-date="*
+        Start_date*"&end-date="*
+        End_date*"&metrics=ga:"*query_metrics*
+        "&dimensions=ga:"*query_dimensions
+
+    requisicao = HTTP.request("GET", url, headers)
+    requisicao_report = HTTP.request("GET", url_get_data, headers)
+    tabela = String(requisicao_report.body)
+    tabela = JSON3.read(tabela)
+    tabela = tabela[:rows]
+
+
+    coltabela = vcat(Dimensions,metrics)
+    df = DataFrame()
+
+    for i in coltabela
+        df[Symbol(i)] = []
+    end
+
+    for i in tabela
+        push!(df, i)
+    end
+    return(df)
 end
-jtable = jsontable(tabela)
-new_dfs = DataFrame(reduce(vcat, jtable))
-println(new_dfs)
+
+dados_ga = ga_extract(id,Start_date,End_date,metrics,Dimensions)
